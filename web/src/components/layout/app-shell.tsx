076f0125402af2320585da3141e1d1c8fb6384e5
connork <amazingcwk2@gmail.com>
2026-03-27 12:12:31 -0700

    feat: add ARIA role to main content area

--- web/src/components/layout/app-shell.tsx
+++ web/src/components/layout/app-shell.tsx
@@ -4,7 +4,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 pl-14 lg:pl-60">
+      <main role="main" className="flex-1 pl-14 lg:pl-60">
         <div className="mx-auto max-w-7xl p-6">{children}</div>
       </main>
     </div>
